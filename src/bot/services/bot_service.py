225d1f9a53ca6a73414f6560ecd291266fad1960
ksamoylov <mebodik@gmail.com>
2021-07-27 01:59:34 +0300

    Added sendMessage method

--- src/bot/services/bot_service.py
+++ src/bot/services/bot_service.py
@@ -1,19 +1,31 @@
-import src.bot.entity.Message
+import requests
+import src.enums.bot_enum as bot_enum
 from src.bot.entity.User import User
 from src.bot.entity.Message import Message
 from src.bot.entity.Chat import Chat
 
 
 def handle_hook_event(data: dict):
-    user, message, chat = create_entities(data)
-    print(user.get_last_name(), message.get_message_id(), chat.get_id())
-    pass
+    user, chat, message = create_entities(data)
+    send_message(message)
 
 
 def create_entities(data: dict) -> set:
-    message_data = data['message']
-    user = User(**message_data['from'])
-    chat = Chat(message_data['chat']['id'], message_data['chat']['type'], user)
-    message = Message(message_data['message_id'], user, chat)
+    message_data = data["message"]
+    user = User(**message_data["from"])
+    chat = Chat(message_data["chat"]["id"], message_data["chat"]["type"], user)
+    message = Message(message_data["message_id"], user, chat)
 
-    return user, message, chat
+    return user, chat, message
+
+
+def send_message(received_message: Message):
+    r = requests.post(
+        f"{bot_enum.API_TELEGRAM_FULL_URL}/{bot_enum.SEND_MESSAGE_METHOD}",
+        {
+            "chat_id": received_message.chat.get_id(),
+            "text": f"Hey, {received_message.user.get_first_name()}!",
+        }
+    )
+
+    print(r.text)
---
+++ src/enums/bot_enum.py
@@ -0,0 +1,6 @@
+from src.helpers.environment_helper import get_env_by_name
+
+API_TOKEN = get_env_by_name('api_token')
+API_TELEGRAM_URL = "https://api.telegram.org"
+API_TELEGRAM_FULL_URL = f"{API_TELEGRAM_URL}/bot{API_TOKEN}"
+SEND_MESSAGE_METHOD = "sendMessage"
